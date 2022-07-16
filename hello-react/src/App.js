f4adeb1cb98abdb2d5d2cd5b429c807b7cbd36e3
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-17 05:10:01 +0900

    프래그먼트 <> </> 표현

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,10 +2,10 @@ import { Fragment } from "react";
 
 function App() {
   return (
-    <Fragment>
+    <>
       <h1>프래그먼트 활용</h1>
-      <h2>prettier가 알아서 문장 디테일 만들어주니까 return 괄호 신경 ㄴㄴ</h2>
-    </Fragment>
+      <h2>프래그먼트 &lt;&gt;와 &lt;/&gt;표현!</h2>
+    </>
   );
 }
 
