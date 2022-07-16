55b790cf0eda7137a2dcda799ff11ba9ae201744
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-17 05:00:41 +0900

    App.js의 내용을 수정함
    프로젝트 생성하는 방법을 기록했다.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>first try in react!</h1>
+      <h2>깃 허브에 연동하고 create-react-app으로 프로젝트 생성함</h2>
+      <h3>create-react-app으로 리액트 프로젝트를 만드는 법!</h3>
+      <ol type="1">
+        <li>먼저 관리자 권한으로 터미널 열기(windows powershell로 해야함.</li>
+        <br></br>
+        <li>터미널에 "yarn create react-app 프로젝트 이름" 입력</li>
+        <br></br>
+        <li>입력하면 react 프로젝트가 만들어지는게 터미널에 뜬다.</li>
+        <br></br>
+        <li>
+          터미널에서 프로젝트로 change directory하고 "yarn start"하여 리액트
+          실행해보면 잘 작동하는 것을 확인 할 수 있다.
+        </li>
+      </ol>
     </div>
   );
 }
