dfc00a8c31294ee62ff1da13d0fbce2577242791
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-17 05:03:44 +0900

    Fragment활용
    Fragment 활용하여 App.js update

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,22 +1,11 @@
+const { Fragment } = require("react");
+
 function App() {
   return (
-    <div>
-      <h1>first try in react!</h1>
-      <h2>깃 허브에 연동하고 create-react-app으로 프로젝트 생성함</h2>
-      <h3>create-react-app으로 리액트 프로젝트를 만드는 법!</h3>
-      <ol type="1">
-        <li>먼저 관리자 권한으로 터미널 열기(windows powershell로 해야함.</li>
-        <br></br>
-        <li>터미널에 "yarn create react-app 프로젝트 이름" 입력</li>
-        <br></br>
-        <li>입력하면 react 프로젝트가 만들어지는게 터미널에 뜬다.</li>
-        <br></br>
-        <li>
-          터미널에서 프로젝트로 change directory하고 "yarn start"하여 리액트
-          실행해보면 잘 작동하는 것을 확인 할 수 있다.
-        </li>
-      </ol>
-    </div>
+    <Fragment>
+      <h1>프래그먼트 활용</h1>
+      <h2>prettier가 알아서 문장 디테일 만들어주니까 return 괄호 신경 ㄴㄴ</h2>
+    </Fragment>
   );
 }
 
