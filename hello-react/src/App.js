044e0192f64d7712467c9503c6786664b295199d
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-17 23:54:08 +0900

    input 태그

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   const name = "리액트";
 
-  return <div className="react">{name}</div>;
+  return (
+    <>
+      <div className="react">{name}</div>
+      <input></input>
+    </>
+  );
 }
 
 export default App;
