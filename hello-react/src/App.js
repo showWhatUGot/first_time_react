5f2741b78aa9688dcd2bc244fe694b4732dd1bae
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-18 14:03:21 +0900

    first props 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,7 @@
-import "./App.css";
+import MyComponent from "./MyComponent";
 
-function App() {
-  const name = "리액트";
-
-  return (
-    <>
-      {/* JSX내부에서 주석달기 */}
-      <div className="react">{name}</div>
-      <input // 태그 안에서는 이렇게
-      ></input>
-      <br /*태그 안에서는 이렇게 2*/></br>
-      <input />
-    </>
-  );
-}
+const App = () => {
+  return <MyComponent name="React" />;
+};
 
 export default App;
