0795cf5ff5c53c001bd67f10568081192287ac85
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-18 17:30:54 +0900

    class형 컴포넌트에서 props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,11 @@
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoriNumber={3}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
