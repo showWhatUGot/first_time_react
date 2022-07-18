0556187c69d0ce5ea80e8976092dbd32fcec4963
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-18 23:36:01 +0900

    state사용법

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,7 @@
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 const App = () => {
-  return (
-    <MyComponent name="React" favoriNumber={3}>
-      리액트
-    </MyComponent>
-  );
+  return <Counter />;
 };
 
 export default App;
