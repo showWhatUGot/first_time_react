ca6ebf0bb576beeddad43d8ee7da92cc8930bb49
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-18 15:29:02 +0900

    propTypes 설정

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,9 +1,11 @@
-const MyComponent = (props) => {
+import PropTypes from "prop-types";
+
+const MyComponent = ({ name, children }) => {
   return (
     <div>
-      안녕하세요! 제 이름은 {props.name}입니다.
+      안녕하세요! 제 이름은 {name}입니다.
       <br />
-      children 값은 {props.children}
+      children 값은 {children}
       입니다.
     </div>
   );
@@ -11,6 +13,10 @@ const MyComponent = (props) => {
 
 MyComponent.defaultProps = {
   name: "기본 이름",
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
 };
 
 export default MyComponent;
