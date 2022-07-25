fe86424e1b0a848750e74308cd3572c0408a4b00
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-25 23:10:57 +0900

    ref사용

--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./ValidationSample.css";
 
 class ValidationSample extends Component {
+  input = React.createRef();
+
   state = {
     password: "",
     clicked: false,
@@ -19,12 +21,14 @@ class ValidationSample extends Component {
       clicked: true,
       validated: this.state.password === "0000",
     });
+    this.input.focus();
   };
 
   render() {
     return (
       <div>
         <input
+          ref={(ref) => (this.input = ref)}
           type="password"
           value={this.state.password}
           onChange={this.handleChange}
