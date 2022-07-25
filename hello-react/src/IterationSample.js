fdd2f7f0e966ce85bdef9c197a5667d6c0620e04
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-07-26 02:53:53 +0900

    concat사용
    push를 사용해서 배열을 업데이트를 하지 않는 이유는 리액트에서는 상태를 업데이트 할때에는 기존의 상태를 유지하고 새로운 상로 설정해야하기 때문이다. 그래서 concat 사용함 뒤에서 자세히 설명

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,9 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", " 눈", " 바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  return <ul>{nameList}</ul>;
-};
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
 
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  const namesList = names.map((names) => <li key={names.id}>{names.text}</li>);
+
+  return (
+    <>
+      <input
+        value={inputText}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      ></input>
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>;
+    </>
+  );
+};
 export default IterationSample;
